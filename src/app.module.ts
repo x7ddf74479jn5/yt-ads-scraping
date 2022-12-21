5d3992ccb3e13302c2f691aabe6447878bffe8f1
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2022-12-22 01:10:56 +0900

    ✨ feat: impl domain and schedule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { YoutubeModule } from './youtube/youtube.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), YoutubeModule],
+  imports: [ConfigModule.forRoot(), ScheduleModule.forRoot(), YoutubeModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/youtube/youtube.module.ts
+++ src/youtube/youtube.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { ScrapingService } from './scraping/scraping.service';
 import { ApiService } from './api/api.service';
 import { DbService } from './db/db.service';
+import { DomainService } from './domain/domain.service';
+import { ScheduleService } from './schedule/schedule.service';
+import { PrismaService } from '@/prisma.service';
 
 @Module({
-  providers: [ScrapingService, ApiService, DbService]
+  providers: [
+    ScrapingService,
+    ApiService,
+    DbService,
+    DomainService,
+    ScheduleService,
+    PrismaService,
+  ],
 })
 export class YoutubeModule {}
